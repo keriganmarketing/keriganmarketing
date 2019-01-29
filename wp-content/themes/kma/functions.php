b881b72b74fc8d97d285c20db9949526453d4b8e
Bryan Baird <bryan@kerigan.com>
2019-01-29 11:08:56 -0500

    merge

--- wp-content/themes/kma/functions.php
+++ wp-content/themes/kma/functions.php
@@ -695,7 +695,7 @@ add_shortcode( 'whitepaper_form', function($atts){
                 ), true
             );
 
-            $successmessage .= '<strong>Thank you for downloading our whitepaper, ' . $whitepaper['name'] . '. You may <a href="' . $whitepaper['name'] . '" >Download it here</a>. We\'ve emailed you a download link as well.</strong>';
+            $successmessage .= 'Thank you for downloading our whitepaper, ' . $whitepaper['name'] . '. You may <a style="text-decoration: underline; font-weight: bold;" href="' . $whitepaper['link'] . '" >Download it here</a>. We\'ve emailed you a download link as well.';
             $showAlert = '<div class="alert alert-success digital-marketing" role="alert">'.$successmessage.'</div>';
 
         } else { // Pass failed. Let's show an error message.
